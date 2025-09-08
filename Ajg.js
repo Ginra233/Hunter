b8ce4c63dd0da492d330ebe0d9c1771a73d45f77
Gibshosting <gibrandwisatrio1305@gmail.com>
2025-09-08 17:24:04 +0700

    Update token list

--- Ajg.js
+++ Ajg.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8224106277:AAET2o_gsYwJCkLgKtHzULrXyl8Eu6eBsyA"
+    "8224106277:AAET2o_gsYwJCkLgKtHzULrXyl8Eu6eBsyA",
+    "7463169022:AAGJR11n-RNC6DWku4ICU9SFBvXSauwmDhE"
   ]
 }
